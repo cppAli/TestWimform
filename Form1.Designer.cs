d32f3c898b7939ecf7afe847479ad9aa44488755
cppAli <romankoalice@gmail.com>
2023-08-26 16:21:34 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace WinFormApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelQuestion = new System.Windows.Forms.Label();
+            this.radioButtonOption1 = new System.Windows.Forms.RadioButton();
+            this.radioButtonOption2 = new System.Windows.Forms.RadioButton();
+            this.radioButtonOption3 = new System.Windows.Forms.RadioButton();
+            this.radioButtonOption4 = new System.Windows.Forms.RadioButton();
+            this.pointplus = new System.Windows.Forms.Label();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.Color.Teal;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.ForeColor = System.Drawing.Color.Teal;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(982, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Aquamarine;
+            this.label1.Location = new System.Drawing.Point(334, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(345, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "!ПРОСТЫЕ ТЕСТЫ!";
+            // 
+            // labelQuestion
+            // 
+            this.labelQuestion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.labelQuestion.Dock = System.Windows.Forms.DockStyle.Top;
+            this.labelQuestion.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelQuestion.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.labelQuestion.Location = new System.Drawing.Point(0, 100);
+            this.labelQuestion.Name = "labelQuestion";
+            this.labelQuestion.Size = new System.Drawing.Size(982, 84);
+            this.labelQuestion.TabIndex = 1;
+            this.labelQuestion.Text = "label2";
+            this.labelQuestion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelQuestion.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // radioButtonOption1
+            // 
+            this.radioButtonOption1.AutoSize = true;
+            this.radioButtonOption1.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButtonOption1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.radioButtonOption1.Location = new System.Drawing.Point(400, 216);
+            this.radioButtonOption1.Name = "radioButtonOption1";
+            this.radioButtonOption1.Size = new System.Drawing.Size(202, 41);
+            this.radioButtonOption1.TabIndex = 2;
+            this.radioButtonOption1.Text = "radioButton1";
+            this.radioButtonOption1.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonOption2
+            // 
+            this.radioButtonOption2.AutoSize = true;
+            this.radioButtonOption2.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButtonOption2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.radioButtonOption2.Location = new System.Drawing.Point(400, 281);
+            this.radioButtonOption2.Name = "radioButtonOption2";
+            this.radioButtonOption2.Size = new System.Drawing.Size(202, 41);
+            this.radioButtonOption2.TabIndex = 3;
+            this.radioButtonOption2.Text = "radioButton2";
+            this.radioButtonOption2.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonOption3
+            // 
+            this.radioButtonOption3.AutoSize = true;
+            this.radioButtonOption3.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButtonOption3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.radioButtonOption3.Location = new System.Drawing.Point(400, 347);
+            this.radioButtonOption3.Name = "radioButtonOption3";
+            this.radioButtonOption3.Size = new System.Drawing.Size(202, 41);
+            this.radioButtonOption3.TabIndex = 4;
+            this.radioButtonOption3.Text = "radioButton3";
+            this.radioButtonOption3.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonOption4
+            // 
+            this.radioButtonOption4.AutoSize = true;
+            this.radioButtonOption4.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButtonOption4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.radioButtonOption4.Location = new System.Drawing.Point(400, 413);
+            this.radioButtonOption4.Name = "radioButtonOption4";
+            this.radioButtonOption4.Size = new System.Drawing.Size(202, 41);
+            this.radioButtonOption4.TabIndex = 5;
+            this.radioButtonOption4.Text = "radioButton4";
+            this.radioButtonOption4.UseVisualStyleBackColor = true;
+            // 
+            // pointplus
+            // 
+            this.pointplus.AutoSize = true;
+            this.pointplus.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.pointplus.ForeColor = System.Drawing.Color.Teal;
+            this.pointplus.Location = new System.Drawing.Point(84, 498);
+            this.pointplus.Name = "pointplus";
+            this.pointplus.Size = new System.Drawing.Size(146, 41);
+            this.pointplus.TabIndex = 6;
+            this.pointplus.Text = "БАЛЛ: 0";
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.BackColor = System.Drawing.Color.Teal;
+            this.buttonNext.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonNext.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.buttonNext.Location = new System.Drawing.Point(414, 493);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(164, 51);
+            this.buttonNext.TabIndex = 7;
+            this.buttonNext.Text = "ВПЕРЕД";
+            this.buttonNext.UseVisualStyleBackColor = false;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Teal;
+            this.button2.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.button2.Location = new System.Drawing.Point(741, 493);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(229, 51);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "ЗАВЕРШИТЬ";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(982, 633);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.pointplus);
+            this.Controls.Add(this.radioButtonOption4);
+            this.Controls.Add(this.radioButtonOption3);
+            this.Controls.Add(this.radioButtonOption2);
+            this.Controls.Add(this.radioButtonOption1);
+            this.Controls.Add(this.labelQuestion);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(8);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelQuestion;
+        private System.Windows.Forms.RadioButton radioButtonOption1;
+        private System.Windows.Forms.RadioButton radioButtonOption2;
+        private System.Windows.Forms.RadioButton radioButtonOption3;
+        private System.Windows.Forms.RadioButton radioButtonOption4;
+        private System.Windows.Forms.Label pointplus;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Button button2;
+        //private System.Windows.Forms.ProgressBar progress;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormApp1
+{
+    public partial class Form1 : Form
+    {
+        public class Question
+        {
+            public string Text { get; set; } //задание
+            public string[] Options { get; set; } //варианты ответов
+            public int CorrectOptionIndex { get; set; } //заложен правильный ответ по индексу
+        }
+        private int progress = 0; //progressbar по выполнению теста
+        private Question[] questions; //
+        private int currentQuestionIndex;
+
+        // Задаем подсчет баллов и его сохранение за пределами метода
+        private int point = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeQuestions();
+            // Начинаем с первого вопроса
+            currentQuestionIndex = 0;
+
+            // Выводим первый вопрос на экран
+            DisplayQuestion();
+
+        }
+
+        private void InitializeQuestions()
+        {
+            // Создаем объекты Question для каждого вопроса
+            questions = new Question[]
+            {
+            new Question
+            {
+                Text = "Вопрос 1:\n Какое вещество является химическим элементом с символом \"H\"?",
+                Options = new string[] { "Гелий", "Гидроген", "Германий", "Гафний" },
+                CorrectOptionIndex = 1
+            },
+            new Question
+            {
+                Text = "Вопрос 2:\n Какая планета Солнечной системы известна своими кольцами?",
+                Options = new string[] { "Земля", "Венера", "Сатурн", "Марс" },
+                CorrectOptionIndex = 2
+            },
+            new Question
+            {
+                Text = "Вопрос 3:\n Сколько сторон у прямоугольника?",
+                Options = new string[] { "3", "4", "5", "6" },
+                CorrectOptionIndex = 0
+            },
+            new Question
+            {
+                Text = "Вопрос 4:\n Какое из чисел является простым?",
+                Options = new string[] { "15", "5", "18", "100" },
+                CorrectOptionIndex = 1
+            },
+            new Question
+            {
+                Text = "Вопрос 5:\n Какой газ необходим растениям для фотосинтеза?",
+                Options = new string[] { "Кислород", "Азот", "Углекислый газ", "Водород" },
+                CorrectOptionIndex = 2
+            },
+            new Question
+            {
+                Text = "Вопрос 6:\n Какое животное известно своим хоботом?",
+                Options = new string[] { "Носорог", "Слон", "Жираф", "Тигр" },
+                CorrectOptionIndex = 1
+            },
+            new Question
+            {
+                Text = "Вопрос 7:\n Сколько зубов у взрослого человека обычно?",
+                Options = new string[] { "32", "28", "20", "36" },
+                CorrectOptionIndex = 0
+            },
+            new Question
+            {
+                Text = "Вопрос 8:\n Кто написал произведение \"Война и мир\"?",
+                Options = new string[] { "Лев Толстой", "Федор Достоевский", "Александр Пушкин", "Николай Гоголь" },
+                CorrectOptionIndex = 0
+            },
+            new Question
+            {
+                Text = "Вопрос 9:\n Какой из этих планет находится ближе всего к Солнцу?",
+                Options = new string[] { "Марс", "Юпитер", "Венера", "Уран" },
+                CorrectOptionIndex = 2
+            },
+            new Question
+            {
+                Text = "Вопрос 10:\n Какая столица Франции?",
+                Options = new string[] { "Белин", "Лондон", "Рим", "Париж" },
+                CorrectOptionIndex = 3
+            },
+            new Question
+            {
+                Text = "Вопрос 11:\n Какое из следующих слов является глаголом?",
+                Options = new string[] { "Стол", "Погода", "Бежать", "Дом" },
+                CorrectOptionIndex = 2
+            },
+            new Question
+            {
+                Text = "Вопрос 12:\n Как называется наибольшее океанское животное?",
+                Options = new string[] { "Синий кит", "Акула", "Медуза", "Дельфин" },
+                CorrectOptionIndex = 0
+            },
+            new Question
+            {
+                Text = "Вопрос 13:\n Какой цвет получится при смешении синего и желтого?",
+                Options = new string[] { "Красный", "Синий", "Зеленый", "Лиловый" },
+                CorrectOptionIndex = 2
+            },
+            new Question
+            {
+                Text = "Вопрос 14:\n Какое из следующих слов является существительным?",
+                Options = new string[] { "Прыгать", "Комод", "Плыть", "Прекрасный" },
+                CorrectOptionIndex = 1
+            },
+            new Question
+            {
+                Text = "Вопрос 15:\n Какая из этих планет не имеет своих естественных спутников (лун)?",
+                Options = new string[] { "Земля", "Марс", "Юпитер", "Венера" },
+                CorrectOptionIndex = 1
+            }
+            };
+        }
+        private void DisplayQuestion()
+        {
+            // Получаем текущий вопрос из массива
+            Question currentQuestion = questions[currentQuestionIndex];
+
+            // Выводим текст вопроса
+            labelQuestion.Text = currentQuestion.Text; //элементу присваиватся объект класса далее указывается какое после конкретно используется
+
+            // Выводим варианты ответов на RadioButton
+            radioButtonOption1.Text = currentQuestion.Options[0]; //к элементу приваиватся значения объекта класс 
+            radioButtonOption2.Text = currentQuestion.Options[1];
+            radioButtonOption3.Text = currentQuestion.Options[2];
+            radioButtonOption4.Text = currentQuestion.Options[3];
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            // Проверяем, был ли выбран правильный ответ
+            Question currentQuestion = questions[currentQuestionIndex];
+            bool isCorrect = false;
+
+            if (radioButtonOption1.Checked && currentQuestion.CorrectOptionIndex == 0)
+                isCorrect = true;
+            else if (radioButtonOption2.Checked && currentQuestion.CorrectOptionIndex == 1)
+                isCorrect = true;
+            else if (radioButtonOption3.Checked && currentQuestion.CorrectOptionIndex == 2)
+                isCorrect = true;
+            else if (radioButtonOption4.Checked && currentQuestion.CorrectOptionIndex == 3)
+                isCorrect = true;
+
+
+            // Проверяем, был ли выбран хотя бы один ответ
+            bool anyOptionSelected = radioButtonOption1.Checked || radioButtonOption2.Checked ||
+                                     radioButtonOption3.Checked || radioButtonOption4.Checked;
+
+            if (!anyOptionSelected)
+            {
+                MessageBox.Show("Выберите ответ перед тем, как продолжить.");
+                return; // Выходим из метода, чтобы не проверять правильность ответа и не переходить к следующему вопросу
+            }
+
+            // Выводим сообщение о результате
+            if (isCorrect)
+            {
+                point++;
+                pointplus.Text = $"БАЛЛ: {point}";
+            }
+            else
+                pointplus.Text = $"БАЛЛ: {point}";
+
+            // Переходим к следующему вопросу или завершаем тестирование
+            if (currentQuestionIndex < questions.Length - 1)
+            {
+                currentQuestionIndex++;
+                DisplayQuestion();
+            }
+            else
+            {
+                MessageBox.Show($" Вы набрали {point} баллов!\n Тестирование завершено!");
+                Close();
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
